5acd9ecbfb508ad80341b780be4eed02e9c3ad74
Echostorm <echostorm@outlook.com>
2026-01-12 14:56:58 -0500

    I know they're going to add more plans so lets make it tweakable now.

--- Flow.Launcher.Plugin.GithubCopilotRequestsCheck/SettingsControl.xaml.cs
+++ Flow.Launcher.Plugin.GithubCopilotRequestsCheck/SettingsControl.xaml.cs
@@ -1,9 +1,11 @@
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Flow.Launcher.Plugin.GithubCopilotRequestsCheck;
 
 public partial class SettingsControl : UserControl
 {
+    private static readonly int[] PredefinedQuotas = { 50, 300, 1000, 1500 };
     public Settings Settings { get; }
 
     public SettingsControl(Settings settings)
@@ -11,9 +13,42 @@ public partial class SettingsControl : UserControl
         Settings = settings;
         InitializeComponent();
         PatPasswordBox.Password = Settings.GitHubPAT;
+        InitializeQuotaSelection();
     }
 
-    private void PatPasswordBox_PasswordChanged(object sender, System.Windows.RoutedEventArgs e)
+    private void InitializeQuotaSelection()
+    {
+        int index = System.Array.IndexOf(PredefinedQuotas, Settings.MonthlyQuota);
+        if (index >= 0)
+        {
+            QuotaComboBox.SelectedIndex = index;
+            CustomQuotaTextBox.Visibility = Visibility.Collapsed;
+        }
+        else
+        {
+            QuotaComboBox.SelectedIndex = 4; // Custom
+            CustomQuotaTextBox.Visibility = Visibility.Visible;
+        }
+    }
+
+    private void QuotaComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (QuotaComboBox.SelectedItem is ComboBoxItem item && item.Tag is string tagStr)
+        {
+            int tag = int.Parse(tagStr);
+            if (tag == 0)
+            {
+                CustomQuotaTextBox.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                CustomQuotaTextBox.Visibility = Visibility.Collapsed;
+                Settings.MonthlyQuota = tag;
+            }
+        }
+    }
+
+    private void PatPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
     {
         Settings.GitHubPAT = PatPasswordBox.Password;
     }
